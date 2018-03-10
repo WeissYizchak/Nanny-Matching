5f3e4baf10c33865d426721b5db5a7f1149cb163
WeissYizchak <35064970+WeissYizchak@users.noreply.github.com>
2018-03-10 22:00:41 +0200

    Add files via upload

---
+++ Mother.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Mother  :Person, INotifyPropertyChanged
+    {
+
+        int _cellphon;
+        public int cellPhone
+        {
+            set
+            {
+               _cellphon = value;
+                //if (value < 100000000 || value > 999999999)
+                //    throw new Exception("it is not a ligal number");
+            }
+            get { return _cellphon; }
+        }
+
+        string _address;
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                _address = value;
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("address cen't to be empty");
+                }
+				if (PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs("Address"));
+			}
+        }
+
+        public string AreaToSearch { get; set; }
+
+		public Day[] timeWorkofWeek = new Day[]
+		 {
+			new Day(),
+			new Day(),
+			new Day(),
+			new Day(),
+			new Day(),
+			new Day()
+		 };
+		public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Remarks { get; set; }
+
+        public override string ToString()
+        {
+            return "name: " + FirstName + " " + LastName + ". \nID: " + Id + ". \nCellPhon number: " + "0" + cellPhone
+                + ". address: " + Address;
+        }
+
+        public Mother Clone()
+        {
+            return (Mother)MemberwiseClone();
+        }
+
+        public Mother() { }
+
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Person
+    {
+        int _id;
+        public int Id
+        {
+            set
+            {
+                if (value < 100000000 || value > 999999999)
+                    throw new Exception("the id shoulde be nine dighit!");
+                _id = value;
+            }
+            get { return _id; }
+        }
+
+        string _lastName;
+        public string LastName
+        {
+            set
+            {
+                _lastName = value;
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("the Lest name con't be empty");
+            }
+            get { return _lastName; }
+        }
+
+        string _firstName;
+        public string FirstName
+        {
+            set
+            {
+                _firstName = value;
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("the First name con't be empty");
+            }
+            get { return _firstName; }
+        }
+    }
+}
